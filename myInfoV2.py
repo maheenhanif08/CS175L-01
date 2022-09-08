03178b7a90bed9cca49b421f55982cf50f46e89f
maheenhanif08 <98927148+maheenhanif08@users.noreply.github.com>
2022-09-08 12:05:11 -0400

    my information v2

---
+++ myInfoV2.py
@@ -0,0 +1,8 @@
+#CS175L-01
+#Maheen Hanif Ghaffar
+#myInfo
+
+print ("Maheen Hanif Ghaffar")
+print ("431 Cedar Avenue, West Long Branch, NJ 07764")
+print ("123-001-5678")
+print ("My college Major is Software Engineering")
