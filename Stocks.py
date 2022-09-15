496b4861e4e42c5563d4d438682cace888ec1a87
maheenhanif08 <98927148+maheenhanif08@users.noreply.github.com>
2022-09-15 09:16:13 -0400

    stocks

--- Stocks.py
+++ Stocks.py
@@ -1,12 +1,19 @@
+#Inputs:
 SharesPurchased = int(2000)
 PricePerShare = float(40.00)
 SharesSold = int(2000)
 PriceSoldPerShare = float(42.75)
 
-OverallPrice = SharesPurchased * PricePerShare
-Commission = OverallPrice * 3/100
+#Calculations:
+PurchasePrice = SharesPurchased * PricePerShare
+CommissionOnPurchase = PurchasePrice * 3/100
 SellingPrice = SharesSold * PriceSoldPerShare
+CommissionOnSale = SellingPrice * 3/100 
+Profit = SellingPrice - PurchasePrice - CommissionOnPurchase - CommissionOnSale
 
-print("The amount of money Joe paid for the stock is:", OverallPrice)
-print("The amount of commission Joe paid his broker when he bought the stock is:", Commission)
-print("The amount for which Joe sold the stock is:", SellingPrice)
+#Print:
+print(f"The amount of money Joe paid for the stock is: ${PurchasePrice:,.2f}")
+print(f"The amount of commission Joe paid his broker when he bought the stock is: ${CommissionOnPurchase:,.2f}")
+print(f"The amount for which Joe sold the stock is: ${SellingPrice:,.2f}")
+print(f"The amount of commission Joe paid his broker when he sold the stock is: ${CommissionOnSale:,.2f}")
+print(f"The profit that Joe had after selling the stocks is: ${Profit:,.2f}")
