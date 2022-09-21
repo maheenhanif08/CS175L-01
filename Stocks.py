e928e393f28391198fad3cbf78dcc71e4aa22af1
maheenhanif08 <98927148+maheenhanif08@users.noreply.github.com>
2022-09-21 18:16:32 -0400

    stocks

--- Stocks.py
+++ Stocks.py
@@ -4,14 +4,16 @@
 #Inputs:
 SharesPurchased = float(input("Enter the number of shares purchased: "))
 PricePerShare = float(input("Enter the price per share: "))
-SharesSold = float(input("Emter the mumber of shares sold: "))
+CommissionPurchase = float(input("Enter the percentage of commission paid on purchase: "))
+SharesSold = float(input("Enter the number of shares sold: "))
 PriceSoldPerShare = float(input("Enter the selling price per share: "))
+CommissionSale = float(input("Enter the percentage of commission paid on sale: "))
 
 #Calculations:
 PurchasePrice = SharesPurchased * PricePerShare
-CommissionOnPurchase = PurchasePrice * 3/100
+CommissionOnPurchase = PurchasePrice * (CommissionPurchase/100)
 SellingPrice = SharesSold * PriceSoldPerShare
-CommissionOnSale = SellingPrice * 3/100 
+CommissionOnSale = SellingPrice * (CommissionSale/100)
 Profit = SellingPrice - PurchasePrice - CommissionOnPurchase - CommissionOnSale
 
 #Print:
