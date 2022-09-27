31139b25f9da8cc7e3fa305d3700fe24471937c6
maheenhanif08 <98927148+maheenhanif08@users.noreply.github.com>
2022-09-27 10:47:34 -0400

    restaurants

--- Restaurants.py
+++ Restaurants.py
@@ -1,20 +1,20 @@
 #Maheen Hanif Ghaffar
 #Restaurants Project
 
-Vegetarian = input("Is anyone in your party a vegetarian? \n")
-Vegan = input("Is anyone in your party a vegan? \n")
-Gluten_Free = input("Is anyone in your party gluten free? \n")
+Vegetarian = input("Is anyone in your party a vegetarian? \n").lower()
+Vegan = input("Is anyone in your party a vegan? \n").lower()
+Gluten_Free = input("Is anyone in your party gluten free? \n").lower()
 
 if Vegetarian == "yes" and Vegan == "yes" and Gluten_Free == "yes":
-    print("Here are your restairant choices: \n  Corner Cafe \n  The Chef's Kitchen")
+    print("Here are your restaurant choices: \n  Corner Cafe \n  The Chef's Kitchen")
 
 
 elif Vegetarian == "yes" and Vegan == "yes" and Gluten_Free == "no":
-    print("Here are your restaurant choices: \n  Corner Cafe \n  The Chefs Kitchen")
+    print("Here are your restaurant choices: \n  Corner Cafe \n  The Chef's Kitchen")
 
 
 elif Vegetarian == "yes" and Vegan == "no" and Gluten_Free == "no":
-    print("Here are your restaurant choices: \n  Main Street Pizza Company \n  Corner Cafe \n  Mama's Fine Italian \n  The Chaf's Kitchen")
+    print("Here are your restaurant choices: \n  Main Street Pizza Company \n  Corner Cafe \n  Mama's Fine Italian \n  The Chef's Kitchen")
 
 
 elif Vegetarian == "no" and Vegan == "no" and Gluten_Free == "no":
