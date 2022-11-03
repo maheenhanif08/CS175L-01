6173eae0330fc2687e04ab934549beee22abfb12
maheenhanif08 <98927148+maheenhanif08@users.noreply.github.com>
2022-11-03 10:12:32 -0400

    Average From Input

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -5,15 +5,29 @@
 def main():
     total = 0
     count = 0
-    numbers_file = open('numbers.txt','r')
     
-    for line in numbers_file:
-        number = float(line)
-        count = count + 1
-        total = total + number
-        print(f'I read in  {count} number(s) Current number is:    {number: .2f} Total is: {total: .2f}')
+    try:
+        numbers_file = open('numbers.txt','r')
+    
+        for line in numbers_file:
+            try:
+                number = float(line)
+            
+            except ValueError:
+                line = line.strip('\n')
+                print("Non-numeric data found in the file: " +line)
+
+            else:
+                count = count + 1
+                total = total + number
+                print(f'I read in  {count} number(s) Current number is:    {number: .2f} Total is: {total: .2f}')
+
+    except IOError:
+        print('An error occured trying to read the file.')
         
     average = total / count
     print(f'Average: {average: .1f}')
+    
+    
 
 main()
